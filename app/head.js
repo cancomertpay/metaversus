15e45a18663ebc42c5d0f510afd4becabc0e4f36
Can Cömertpay <cancomertpay@gmail.com>
2023-09-04 20:16:54 +0300

    #Change titles

--- app/head.js
+++ app/head.js
@@ -1,6 +1,6 @@
 const Head = () => (
   <>
-    <title>War and Wisdom</title>
+    <title>CC-Metaversus</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="/favicon.ico" />
   </>
