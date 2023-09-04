fd6b8af682fff4b503744e8d443f082ae34416e6
Can Cömertpay <cancomertpay@gmail.com>
2023-09-04 14:05:49 +0300

    #Add get started section and start steps article

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import { Footer, Navbar } from '../components';
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
 
 const Page = () => (
-  <div className='bg-white overflow-hidden'>
+  <div className='bg-primary-black overflow-hidden'>
     <Navbar />
     <Hero />
     <div className="relative">
